04428432261a3b04ddfbb21bb27e76115ac6d7fb
alexafonsodossantos <aa.santos95@gmail.com>
2022-08-12 16:53:48 -0300

    Exercício 3 - Soma

---
+++ ListaDeExercícios/EstruturaSequencial/ex03.py
@@ -0,0 +1,11 @@
+#Faça um Programa que peça dois números e imprima a soma.
+try:
+    n1 = int(input("Digite o primeiro número: "))
+    n2 = int(input("Digite o segundo número: "))
+    s = n1 + n2
+    print("Resultado soma:", s)
+except:
+    print("Entrada inválida.")
+
+
+
