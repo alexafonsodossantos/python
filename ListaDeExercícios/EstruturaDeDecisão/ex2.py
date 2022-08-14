8313149e34e7078b9fa3583eefab491b9e2ff24b
alexafonsodossantos <aa.santos95@gmail.com>
2022-08-14 13:15:59 -0300

    Exercício 02 - Positivo e Negativo

---
+++ ListaDeExercícios/EstruturaDeDecisão/ex2.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça um valor e 
+# mostre na tela se o valor é positivo ou negativo.
+
+n = int(input("Digite um número "))
+if n < 0:
+    print("O número é negativo.")
+else:
+    print("O número é positivo.")
