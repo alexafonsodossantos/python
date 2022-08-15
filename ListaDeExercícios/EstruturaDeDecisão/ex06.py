4f0e6c27dff7fe8a9fa93a69473bd99a3292b394
alexafonsodossantos <aa.santos95@gmail.com>
2022-08-15 16:43:02 -0300

    Exercício 06 - Maior e Menor número

---
+++ ListaDeExercícios/EstruturaDeDecisão/ex06.py
@@ -0,0 +1,24 @@
+n1 = float(input("Digite o 1º número: "))
+n2 = float(input("Digite o 2º número: "))
+n3 = float(input("Digite o 3º número: "))
+
+if n1 > n2 and n1 > n3:
+    print("Maior número: ", n1)
+
+if n2 > n1 and n2 > n3:
+    print("Maior número: ", n2)
+
+if n3 > n1 and n3 > n2:
+    print("Maior número: ", n3)
+
+if n1 < n2 and n1 < n3:
+    print("Menor número: ", n1)
+
+if n2 < n1 and n2 < n3:
+    print("Menor número: ", n2)
+
+if n3 < n1 and n3 < n2:
+    print("Menor número: ", n3)
+
+
+
