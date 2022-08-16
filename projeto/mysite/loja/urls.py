83576eb59dc2d87ae7d3d36de6add9f46b27baad
alexafonsodossantos <aa.santos95@gmail.com>
2022-08-16 15:50:25 -0300

    added a few templates, and a base html

--- projeto/mysite/loja/urls.py
+++ projeto/mysite/loja/urls.py
@@ -8,5 +8,4 @@ app_name = 'loja'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:produto_id>/', views.detail, name='detail'),
-    path('<int:user_id>/', views.profile, name='profile'),
 ]
--- projeto/mysite/mysite/urls.py
+++ projeto/mysite/mysite/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('loja/', include('loja.urls', namespace="loja")),
-    path('profile/', include('loja.urls', namespace="profile")),
+
 ]
