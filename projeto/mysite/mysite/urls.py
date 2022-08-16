cd0c5ab4587f242a23c6073f356c32fff9b536a6
alexafonsodossantos <aa.santos95@gmail.com>
2022-08-16 11:44:32 -0300

    added url namespaces

--- projeto/mysite/mysite/urls.py
+++ projeto/mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('loja/', include('loja.urls')),
+    path('loja/', include('loja.urls', namespace="loja")),
     path('admin/', admin.site.urls),
 ]
