aab1076d6030c4c44d1fede40b0b30f117be3ca5
alexafonsodossantos <aa.santos95@gmail.com>
2022-08-17 23:36:24 -0300

    more css...

--- projeto/mysite/mysite/settings.py
+++ projeto/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4$kq@(moxzn8c@e^b(jmvuea6$f_tnw^2881spz_7pcvqh%(4l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.15.10']
 
 
 # Application definition
