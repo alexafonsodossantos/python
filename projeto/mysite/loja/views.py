35137119a838d701bde0492e201aa6dbeb52cf2f
alexafonsodossantos <aa.santos95@gmail.com>
2022-08-23 12:25:28 -0300

    just in case

--- projeto/mysite/loja/views.py
+++ projeto/mysite/loja/views.py
@@ -38,12 +38,11 @@ def cart_add(request):
     p = Produto.objects.get(id= request.POST.get('product_id'))
     q = request.POST.get('qtd')
    
-    if Cart.objects.get(username=u, produtos_id=p ):
+    try:
         cart_exists = Cart.objects.get(username=u, produtos_id=p )
         update_cart = Cart(pk=int(cart_exists.pk), username=u, produtos_id=p, qtd = cart_exists.qtd + int(q))
         update_cart.save()
-        
-    else:
+    except:
         cart_instance = Cart.objects.create(username=u, produtos_id = p, qtd=q)
    
     return redirect('/loja/cart/'+str(u))
