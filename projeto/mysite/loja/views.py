2719d2f36bfc47321a95a9f9ef1a2ffd8dab6bce
alexafonsodossantos <aa.santos95@gmail.com>
2022-08-23 12:11:28 -0300

    fixed cart qtd

--- projeto/mysite/loja/views.py
+++ projeto/mysite/loja/views.py
@@ -37,5 +37,13 @@ def cart_add(request):
     u = User.objects.get(username= request.POST.get('username'))
     p = Produto.objects.get(id= request.POST.get('product_id'))
     q = request.POST.get('qtd')
-    cart_instance = Cart.objects.create(username=u, produtos_id = p, qtd=q)
+   
+    if Cart.objects.get(username=u, produtos_id=p ):
+        cart_exists = Cart.objects.get(username=u, produtos_id=p )
+        update_cart = Cart(pk=int(cart_exists.pk), username=u, produtos_id=p, qtd = cart_exists.qtd + int(q))
+        update_cart.save()
+        
+    else:
+        cart_instance = Cart.objects.create(username=u, produtos_id = p, qtd=q)
+   
     return redirect('/loja/cart/'+str(u))
