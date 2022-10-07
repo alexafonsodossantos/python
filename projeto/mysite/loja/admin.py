5c84ddff675862786a5dc47bf43f34a33277ef17
Alex Afonso <60831019+alexafonsodossantos@users.noreply.github.com>
2022-10-07 09:05:23 -0300

    Delete projeto directory

--- projeto/mysite/loja/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-# Register your models here.
-from .models import Produto, Cart, Categorias
-admin.site.register(Produto)
-admin.site.register(Cart)
-admin.site.register(Categorias)
